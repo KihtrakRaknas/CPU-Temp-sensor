d09020fde10de572243b24cf49f844d499abfd37
Usharani Guduri <usharaniguduri@Usharanis-MacBook-Air.local>
2019-07-17 18:35:49 -0400

    optimied for pi

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const si = require('systeminformation');
 
-si.cpuTemperature()
+setInterval(()=>{
+    si.cpuTemperature()
     .then(data => console.log(data))
     .catch(error => console.error(error));
 
     //Linux
     //sudo apt-get install lm-sensors
+},10000)
